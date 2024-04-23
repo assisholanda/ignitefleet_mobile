d9f23298e42492e1b27699b5a544f4456165b429
assisholanda <assisholandal@gmail.com>
2024-04-22 21:29:43 -0300

    criado a primeia parte de configuracao dos tokens de acesso junto ao mongo db atlas

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,59 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
 import { Container, Title, Slogam} from './styles';
+
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from '@env';
 
 import backgroundImg from '../../assets/background.png';
 
 import { Button } from '../../components/Button';
 
+GoogleSignin.configure({
+    scopes: ['email', 'profile'],
+    webClientId: WEB_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+});
+
 export function SignIn() {
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() {
+      
+    try {
+      setIsAuthenticating(true);
+
+      const { idToken } = await GoogleSignin.signIn();
+
+      if(idToken) {
+        
+      } else {
+        Alert.alert("Login", "Não foi possível logar e recuperar seus dados da sua conta Google.");
+        setIsAuthenticating(false);
+      }
+     
+      
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Login", "Não foi possível conectar-se a sua conta Google.");
+      setIsAuthenticating(false);
+    }
+
+
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogam>Gestão de uso de veículos</Slogam>
 
-      <Button title='Entrar com o google' />
+      <Button 
+        title='Entrar com o google' 
+        isLoading={isAuthenticating} 
+        onPress={handleGoogleSignIn}
+      />
 
     </Container>
   );
